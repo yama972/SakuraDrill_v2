686bc56eaca4d6f546a0afa7a4467122d7f68cdc
yama972 <r.yamaguchi3513@gmail.com>
2026-09-10 04:31:01 +0900

    「v7.7 Stable_09_10」に変更
    「★★★★TEST★★★★」の表示を
    v7.7 Stable_09_10に変更。

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ Phase1
 /////////////////////////////////////////////////////
 
 const APP_NAME = "🌸 SakuraDrill";
-const APP_VERSION = "★★★★TEST★★★★";
+const APP_VERSION = "v7.7 Stable_09_10";
 const dailyMessages = [
     "🌸 今日も一歩ずつ進もう！",
     "😊 まちがえても大丈夫！",
